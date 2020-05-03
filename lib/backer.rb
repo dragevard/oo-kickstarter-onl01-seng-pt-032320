e3181f0693961738212862c25d0e59328993be0d
Derek phung <dragevards@gmail.com>
2020-05-03 23:02:57 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,15 +1,15 @@
 class Backer
-  
-  attr_reader :backed_projects
-  attr_accessor :name 
-  
-  def intialize(name)
-    @name = name 
-    @backed_projects = []
-  end 
-  
-  def back_project(project)
-    backed_projects << project 
-    project.backers << self
-  end 
+
+    attr_reader :name
+    attr_accessor :backed_projects
+
+    def initialize(name)
+        @name = name
+        @backed_projects = []
+    end
+
+    def back_project(project)
+        @backed_projects << project
+        project.backers << self
+    end
 end
